3d522c696c1416a5407ed673be501d7c3343b34a
Brandon LaFave <blafave@hollandamerica.com>
2019-05-11 15:38:00 -0700

    Add eslint and nodemon to project

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Test landing page for API');
+  res.send('Test landing page for API');
 });
 
 app.listen(port, () => {
-    console.log(`Running on port ${port}`);
+  console.log(`Running on port ${port}`);
 });
